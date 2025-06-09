e5fcc25a6de183830cd7c93df21bf13096999843
Edgarbriceno26@outlook.es <Edgarbriceno26@outlook.es>
2025-06-08 22:41:46 -0400

    la configuracion carga desde el localstore

--- src/js/start.js
+++ src/js/start.js
@@ -1,8 +1,31 @@
 let countdownInterval;
+let pairCount = document.getElementById("pairCount");
+let time = document.getElementById("timeLimit");
+let omitLetters = document.getElementById("omitLetters");
+let omitPairs = document.getElementById("omitPairs");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const omitted = localStorage.getItem("omitted");
+    const omittedPair = JSON.parse(localStorage.getItem("omittedPairs"));
+    const count = localStorage.getItem("count");
+    const timeLimit = localStorage.getItem("time");
+
+    if (omitted) {
+        omitLetters.value = omitted;
+    }
+    if (omittedPair) {
+        omitPairs.value = omittedPair.join(" ");
+    }
+    if (count) {
+        pairCount.value = count;
+    }
+    if (timeLimit) {
+        time.value = timeLimit;
+    }
+});
 
 
 function startTest() {
-    console.log("Test started");
     const count = parseInt(document.getElementById("pairCount").value);
     const time = parseInt(document.getElementById("timeLimit").value);
     const omitted = document.getElementById("omitLetters").value.toUpperCase().replace(/[^A-Z]/g, "").split("");
